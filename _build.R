28500fbf8cf70b31cecdf8eee9b619b8a44cd1c3
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-07-20 20:50:25 +0200

    chore: rebuild using submodules

--- _build.R
+++ _build.R
@@ -5,11 +5,15 @@ devnull <- sapply(
   X = sapply(
     X = list.files("xaringan-remarkjs", full.names = TRUE),
     FUN = list.files,
-    pattern = "\\.Rmd$",
+    pattern = "\\.rmd$",
     full.names = TRUE
   ),
   FUN = function(itemplate) {
-    ihtml <- sub("\\.Rmd", "\\.html", sub("-.*/", "-", itemplate))
+    ihtml <- paste0(
+      sub("-.*", "-", itemplate),
+      sub(".*-", "", basename(dirname(itemplate))),
+      ".html"
+    )
     out <- rmarkdown::render(
       input = itemplate,
       output_file = ihtml,
@@ -34,22 +38,22 @@ devnull <- sapply(
   ),
   FUN = function(itemplate) {
     quarto::quarto_render(input = itemplate, quiet = TRUE)
-    ihtml <- sub("\\.qmd", "\\.html", itemplate)
+    png <- paste0(
+      sub("-.*", "-", itemplate),
+      sub(".*-", "", basename(dirname(itemplate))),
+      ".png"
+    )
+    ihtml <- sub(".qmd", ".html", itemplate)
+
     webshot2::webshot(
       url = ihtml,
-      file = file.path(
-        "thumbs",
-        sub("\\.html", ".png", sub("\\.qmd", "\\.html", sub("-.*/", "-", itemplate)))
-      ),
+      file = file.path("thumbs", png),
       vwidth = 1920,
       vheight = 1080
     )
     file.copy(
       from = ihtml,
-      to = file.path(
-        "docs",
-        sub("\\.qmd", "\\.html", sub("-.*/", "-", itemplate))
-      ),
+      to = file.path("docs", sub(".png", ".html", png)),
       overwrite = TRUE
     )
     unlink(x = ihtml, force = TRUE)
